3f89f6c2f9845176a484ab81a53494b8072e3b8f
Mateus Galdino Barros <edwardaleatorio@gmail.com>
2026-03-22 15:05:17 -0300

    Documentação para estudo

--- src/ConexaoDB.java
+++ src/ConexaoDB.java
@@ -8,13 +8,15 @@ public class ConexaoDB {
     private static final String USUARIO = "root";
     private static final String SENHA = "2024";
 
+    //Parte do codigo responsavel por guardar as credenciais do banco de dados que vai ser utilizado
+
     public static Connection conectar() {
         try {
-            Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+            Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA); //Cria uma variavel de conexão que utiliza o DriverManager para estabelecer um canal de comunicação entre o java e o MySQL
             System.out.println("Conectado ao banco");
             return conexao;
         } catch (SQLException e) {
-            System.out.println("Erro ao conectar: " + e.getMessage());
+            System.out.println("Erro ao conectar: " + e.getMessage()); //Executa em caso de o java não conseguir se conectar com sucesso mostrando o erro
             return null;
         }
     }
--- src/TarefaDAO.java
+++ src/TarefaDAO.java
@@ -7,18 +7,18 @@ public class TarefaDAO {
 
     public TarefaDAO(Connection conexao) {
         this.conexao = conexao;
-    }
+    } //Estabelece a conexão com o banco de dados usando o DriverManager
 
     public void adicionar(Tarefa tarefa) {
-        String sql = "INSERT INTO tarefas (descricao, status) VALUES (?,?)";
+        String sql = "INSERT INTO tarefas (descricao, status) VALUES (?,?)"; //Guarda a query sql em uma string
 
         try {
-            PreparedStatement stmt = conexao.prepareStatement(sql);
-            stmt.setString(1, tarefa.getDescricao());
+            PreparedStatement stmt = conexao.prepareStatement(sql);  //Usa a conexão para mandar a string para o MySQL e informa que vai usa-la
+            stmt.setString(1, tarefa.getDescricao());  //Usa o objeto enviado como parametro para comunicar quais os valores que vão ser enviados para o sql para preencher as lacunas
             stmt.setString(2, tarefa.getStatus());
-            stmt.executeUpdate();
+            stmt.executeUpdate(); //Executa o update (Cria uma tarefa nova no banco de dados
         }catch (SQLException e) {
-            System.out.println("Erro ao adicionar: "+e.getMessage());
+            System.out.println("Erro ao adicionar: "+e.getMessage()); //Pega o erro em caso de não conseguir adicionar
         }
     }
 
@@ -27,7 +27,7 @@ public class TarefaDAO {
 
         try {
             PreparedStatement stmt = conexao.prepareStatement(sql);
-            stmt.setInt(1, tarefa.getId());
+            stmt.setInt(1, tarefa.getId()); //Pega o ID do objeto informado para remover a taefa no banco de dados
             stmt.executeUpdate();
             System.out.println("Tarefa removida com sucesso");
         } catch (SQLException e) {
@@ -50,16 +50,16 @@ public class TarefaDAO {
 
     public ArrayList<Tarefa> listar() {
         String sql = "SELECT * FROM tarefas";
-        ArrayList<Tarefa> tarefas = new ArrayList<>();
+        ArrayList<Tarefa> tarefas = new ArrayList<>();//Cria um arrayList que vai ficar responsavel por guardar os dados enviados pelo sql
 
         try {
             PreparedStatement stmt = conexao.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
+            ResultSet rs = stmt.executeQuery(); //Cria uma variavel que executa a query e retorna dados
 
-            while (rs.next()) {
-                Tarefa t = new Tarefa(rs.getString("descricao"), rs.getString("status"));
+            while (rs.next()) { //Passa o cursor no banco de dados e informa se existe mais uma linha no banco de dados
+                Tarefa t = new Tarefa(rs.getString("descricao"), rs.getString("status")); // Cria um objeto Tarefa com os valores da linha atual do banco
                 t.setId(rs.getInt("id"));
-                tarefas.add(t);
+                tarefas.add(t); //Adiciona as tarefas no arrayList
             }
 
         }catch (SQLException e) {
@@ -68,7 +68,7 @@ public class TarefaDAO {
         return tarefas;
     }
 
-    public ArrayList<Tarefa> filtro(Tarefa tarefa) {
+    public ArrayList<Tarefa> filtro(Tarefa tarefa) { //Faz a mesma coisa da função listar porém usa uma linha para definir o status a ser filtrado
         String sql = "SELECT * FROM tarefas WHERE status = ?";
         ArrayList<Tarefa> tarefas = new ArrayList<>();
 
